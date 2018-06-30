f17f24f1c70040bfdd56acc745ed95e1194952b7
plutoghost <plutoghost@qq.com>
2018-06-30 16:41:37 +0800

    Move pptp server from vpn to services page

--- package/lean/luci-app-pptp-server/luasrc/controller/pptp-server.lua
+++ package/lean/luci-app-pptp-server/luasrc/controller/pptp-server.lua
@@ -6,9 +6,7 @@ function index()
 		return
 	end
 	
-	entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
 	
-	local page
 
-	entry({"admin", "vpn", "pptp-server"}, cbi("pptp-server/pptp-server"), _("PPTP VPN Server"), 80).dependent=false
+	entry({"admin", "services", "pptp-server"}, cbi("pptp-server/pptp-server"), _("PPTP VPN Server"), 80).dependent=false
 end
